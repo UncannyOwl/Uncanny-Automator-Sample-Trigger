c380aceb9fef6a4c90ecaf8c8c76a34739f0038f
Saad Siddique <34320771+uncannysaad@users.noreply.github.com>
2021-05-11 00:04:16 -0400

    Initial push

---
+++ uncanny-automator-trigger-only.php
@@ -0,0 +1,13 @@
+<?php // phpcs:ignore WordPress.Files.FileName.InvalidClassFileName
+
+/**
+ * @wordpress-plugin
+ * Plugin Name:       Uncanny Automator Sample Trigger
+ * Plugin URI:        https://www.automatorplugin.com
+ * Description:       Sample trigger for Uncanny Automator
+ * Version:           1.0.0
+ * Author:            Uncanny Automator
+ * Author URI:        https://www.automatorplugin.com
+ * License:           GPL-3.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ */
