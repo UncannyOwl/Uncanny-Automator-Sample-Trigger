a53f394b20842d755c064070f2f4b5c0755058ab
Saad Siddique <34320771+uncannysaad@users.noreply.github.com>
2021-05-11 04:41:43 -0400

    Initial commit

--- uncanny-automator-trigger-only.php
+++ uncanny-automator-trigger-only.php
@@ -11,3 +11,25 @@
  * License:           GPL-3.0+
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
  */
+class Uncanny_Automator_Trigger_Only {
+	/**
+	 * Uncanny_Automator_Trigger_Only constructor.
+	 */
+	public function __construct() {
+		add_filter( 'automator_integrations', array( $this, 'load_triggers' ) );
+	}
+
+	/**
+	 * @param $integrations
+	 *
+	 * @return array|mixed
+	 */
+	public function load_triggers( $integrations ) {
+		$add_to_integration = 'uncanny-automator';
+		$trigger            = __DIR__ . '/uoa-recipenotcompleted.php';
+
+		return automator_add_trigger( $integrations, $trigger, $add_to_integration );
+	}
+}
+
+new Uncanny_Automator_Trigger_Only();
